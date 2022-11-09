ab0dd3bcabac42e34f0f0e8bd5c2013f2cad4948
JULES ROBINSON <julianna_85@icloud.com>
2022-11-08 21:19:57 -0600

    Added movie search api to database structure

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem "puma", "~> 5.0"
  gem 'jwt'
 
 gem "jb"
+
+gem "http"
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,11 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies.as_json
   end
+
+  def search
+    response = HTTP.get("http://www.omdbapi.com/?t=#{params[:t]}&apikey=#{Rails.application.credentials.movies_search_api_key}")
+    data = response.parse
+    # movies = data["movies"]
+    render json: data
+  end
 end
